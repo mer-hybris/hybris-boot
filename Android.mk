e74593c4e9b788a63ff3effbd5543cfb1c881411
Dmitry Panfilov <energycsdx@gmail.com>
2014-10-12 22:51:03 +0300

    Look for HYBRIS_BOOT_PART and HYBRIS_DATA_PART only if hybris-updater-script is built

--- Android.mk
+++ Android.mk
@@ -39,25 +39,6 @@ HYBRIS_R_ALWAYSDEBUG := 1
 
 HYBRIS_FIXUP_MOUNTS := $(LOCAL_PATH)/fixup-mountpoints
 
-
-# Find any fstab files for required partition information.
-# in AOSP we could use TARGET_VENDOR
-# TARGET_VENDOR := $(shell echo $(PRODUCT_MANUFACTURER) | tr '[:upper:]' '[:lower:]')
-# but Cyanogenmod seems to use device/*/$(TARGET_DEVICE) in config.mk so we will too.
-HYBRIS_FSTABS := $(shell find device/*/$(TARGET_DEVICE) -name *fstab* | grep -v goldfish)
-
-# Get the unique /dev field(s) from the line(s) containing the fs mount point
-# Note the perl one-liner uses double-$ as per Makefile syntax
-HYBRIS_BOOT_PART := $(shell /usr/bin/perl -w -e '$$fs=shift; while (<>) { next unless /^$$fs\s|\s$$fs\s/;for (split) {next unless m(^/dev); print "$$_\n"; }}' /boot $(HYBRIS_FSTABS) | sort -u)
-HYBRIS_DATA_PART := $(shell /usr/bin/perl -w -e '$$fs=shift; while (<>) { next unless /^$$fs\s|\s$$fs\s/;for (split) {next unless m(^/dev); print "$$_\n"; }}' /data $(HYBRIS_FSTABS) | sort -u)
-
-$(warning ********************* /boot appears to live on $(HYBRIS_BOOT_PART))
-$(warning ********************* /data appears to live on $(HYBRIS_DATA_PART))
-
-ifneq ($(words $(HYBRIS_BOOT_PART))$(words $(HYBRIS_DATA_PART)),11)
-$(error There should be a one and only one device entry for HYBRIS_BOOT_PART and HYBRIS_DATA_PART)
-endif
-
 # Command used to make the image
 MKBOOTIMG := mkbootimg
 BB_STATIC := $(PRODUCT_OUT)/utilities/busybox
@@ -187,7 +168,23 @@ UPDATER_INTERMEDIATE := $(call intermediates-dir-for,ROOT,$(LOCAL_MODULE),)
 
 UPDATER_SCRIPT_SRC := $(LOCAL_PATH)/updater-script
 
+# Find any fstab files for required partition information.
+# in AOSP we could use TARGET_VENDOR
+# TARGET_VENDOR := $(shell echo $(PRODUCT_MANUFACTURER) | tr '[:upper:]' '[:lower:]')
+# but Cyanogenmod seems to use device/*/$(TARGET_DEVICE) in config.mk so we will too.
+HYBRIS_FSTABS := $(shell find device/*/$(TARGET_DEVICE) -name *fstab* | grep -v goldfish)
+
+# Get the unique /dev field(s) from the line(s) containing the fs mount point
+# Note the perl one-liner uses double-$ as per Makefile syntax
+HYBRIS_BOOT_PART = $(shell /usr/bin/perl -w -e '$$fs=shift; while (<>) { next unless /^$$fs\s|\s$$fs\s/;for (split) {next unless m(^/dev); print "$$_\n"; }}' /boot $(HYBRIS_FSTABS) | sort -u)
+HYBRIS_DATA_PART = $(shell /usr/bin/perl -w -e '$$fs=shift; while (<>) { next unless /^$$fs\s|\s$$fs\s/;for (split) {next unless m(^/dev); print "$$_\n"; }}' /data $(HYBRIS_FSTABS) | sort -u)
+
 $(LOCAL_BUILT_MODULE): $(UPDATER_SCRIPT_SRC)
+	$(warning ********************* /boot appears to live on $(HYBRIS_BOOT_PART))
+	$(warning ********************* /data appears to live on $(HYBRIS_DATA_PART))
+	ifneq ($(words $(HYBRIS_BOOT_PART))$(words $(HYBRIS_DATA_PART)),11)
+	$(error There should be a one and only one device entry for HYBRIS_BOOT_PART and HYBRIS_DATA_PART)
+	endif
 	@echo "Installing updater .zip script resources."
 	mkdir -p $(dir $@)
 	rm -rf $@

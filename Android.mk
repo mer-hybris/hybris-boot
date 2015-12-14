e59043eb19c1d54fe93b49e72b52a86dbbca9619
Martin Ghosal <martin_ghosal@hotmail.co.uk>
2015-12-14 21:48:15 +0000

    Force 32bit build if Android SDK is arm64

--- Android.mk
+++ Android.mk
@@ -256,7 +256,12 @@ $(LOCAL_BUILT_MODULE): $(UPDATER_UNPACK_SRC)
 
 HYBRIS_UPDATER_UNPACK := $(LOCAL_BUILD_MODULE)
 
+.PHONY: hybris-hal hybris-common
 
-.PHONY: hybris-hal
-hybris-hal: bootimage hybris-updater-unpack hybris-updater-script hybris-recovery hybris-boot linker init libc adb adbd libEGL libGLESv1_CM libGLESv2 servicemanager logcat updater
+hybris-common: bootimage hybris-updater-unpack hybris-updater-script hybris-recovery hybris-boot servicemanager logcat updater init adb adbd
 
+ifeq ("$(TARGET_ARCH)", "arm64")
+hybris-hal: hybris-common linker_32 libc_32 libEGL_32 libGLESv1_CM_32 libGLESv2_32 
+else
+hybris-hal: hybris-common linker libc libEGL libGLESv1_CM libGLESv2
+endif
